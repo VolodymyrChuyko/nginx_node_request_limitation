40960263c18656c068ce6a91ff35f39b9d9bfa5e
Volodymyr Chuiko <volodymyr.chuiko@clean.io>
2024-03-09 19:07:18 +0200

    Add node server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
-import { sayHi } from "./components";
+'use strict';
+import { Request, Response } from 'express';
+import express from 'express';
 
-console.log('Hello, world!');
+// Service
+function generateServerResponse(responseLimit: string) {
+  return `The current endpoint request limitation is ${responseLimit}`;
+}
 
-sayHi('World');
+// Controller
+function noLimit(req: Request, res: Response) {
+  res.end(generateServerResponse('none'));
+}
+
+function limit10(req: Request, res: Response) {
+  res.end(generateServerResponse('10'));
+}
+
+function limit100(req: Request, res: Response) {
+  res.end(generateServerResponse('100'));
+}
+
+// Router
+const smileStationRouter = express.Router();
+smileStationRouter.get('/', noLimit);
+smileStationRouter.get('/1l', limit10);
+smileStationRouter.get('/1h', limit100);
+
+// Server
+export const createServer = () => {
+  const app = express();
+
+  app.use('/smile-station', smileStationRouter);
+
+  return app;
+};
+
+// Start server
+const port = 3001;
+const server = createServer();
+server.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
