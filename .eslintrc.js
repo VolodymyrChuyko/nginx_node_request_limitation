a11076daa95f1cb6b457ac09851636c1b732bdbf
Volodymyr Chuiko <volodymyr.chuiko@clean.io>
2024-03-09 19:06:57 +0200

    Update configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,6 @@ module.exports = {
     project: ['./tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
   rules: {
     'semi': 'error',
   }
